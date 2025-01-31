661579e820b1cba543d0e9c09d9f9d6c9dfe1dc5
Héctor Soler <hecalana@gmail.com>
2025-01-31 12:46:05 -0500

    refactor: Separate logic to reset forms

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common'
-import { Component, inject } from '@angular/core'
+import { Component, inject, ViewChild } from '@angular/core'
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButtonModule, MatIconButton } from '@angular/material/button'
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog'
 import { MatIcon, MatIconModule } from '@angular/material/icon'
-import { MatStepperModule } from '@angular/material/stepper'
+import { MatStepper, MatStepperModule } from '@angular/material/stepper'
 import { MatListSubheaderCssMatStyler } from '@angular/material/list'
 import { DropzoneTableComponent } from './dropzone-table/dropzone-table.component'
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper'
@@ -41,24 +41,14 @@ export class ModalComponent {
   headers: string[] = []
   ourColumnHeaders = ['Identifier', 'First Name', 'Last Name', 'Narrative', 'Evidence Narrative', 'Evidence URL', 'Issue Date', 'Expiration Date']
   allSelectsValid = false
+  @ViewChild('stepper') stepper!: MatStepper
 
   protected readonly formBuilder = inject(FormBuilder)
 
-  firstFormGroup = this.formBuilder.group({
-    firstCtrl: ['', Validators.required]
-  })
-
-  secondFormGroup = this.formBuilder.group({
-    firstCtrl: ['', Validators.required]
-  })
-
-  thirdFormGroup = this.formBuilder.group({
-    firstCtrl: ['', Validators.required]
-  })
-
-  fourthFormGroup = this.formBuilder.group({
-    firstCtrl: ['', Validators.required]
-  })
+  firstFormGroup = this.formBuilder.group({})
+  secondFormGroup = this.formBuilder.group({})
+  thirdFormGroup = this.formBuilder.group({})
+  fourthFormGroup = this.formBuilder.group({})
 
   constructor (
     private readonly dialogRef: MatDialogRef<ModalComponent>) {
@@ -68,10 +58,35 @@ export class ModalComponent {
     this.isFileUploaded = data.length > 0
     this.csvData = data
     this.headers = data.length > 0 ? Object.values(data[0]) : []
+    this.resetStepper()
   }
 
   handleValidSelects (valid: boolean) {
     this.allSelectsValid = valid
+  }
+
+  resetStepper () {
+    this.stepper.reset()
+    this.resetSecondFormGroup()
+    this.resetThirdFormGroup()
+    this.resetFourthFormGroup()
+  }
+
+  resetSecondFormGroup () {
+    this.secondFormGroup.reset()
+    this.secondFormGroup = this.formBuilder.group({})
+    this.headers.forEach((_, index) => {
+      this.secondFormGroup.addControl(`select${index}`, this.formBuilder.control('-1', Validators.required))
+    })
+    this.allSelectsValid = false
+  }
+
+  resetThirdFormGroup () {
+    this.thirdFormGroup.reset()
+  }
+
+  resetFourthFormGroup () {
+    this.thirdFormGroup.reset()
   }
 
   onSave (): void {
